b6cb92350c5c98c08d61ad5087e04984015e9920
Abdelghafour2001 <abdelghafourlahrache@gmail.com>
2023-05-26 22:46:30 +0200

    updated the blog beans and controllers

---
+++ src/main/java/com/example/model/WatchList.java
@@ -0,0 +1,22 @@
+package com.example.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class WatchList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long watchId;
+    private String name;
+    private String url;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+    private String anime_id;
+}
---
+++ src/main/java/com/example/service/WatchListService.java
@@ -0,0 +1,37 @@
+package com.example.service;
+
+import com.example.model.History;
+import com.example.model.User;
+import com.example.model.WatchList;
+import com.example.repository.HistoryRepository;
+import com.example.repository.UserRepository;
+import com.example.repository.WatchListRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+@Service
+@AllArgsConstructor
+@Slf4j
+@Transactional
+
+public class WatchListService {
+    private final WatchListRepository watchRepository;
+    private final UserRepository userRepository;
+
+    public List<WatchList> getWatchListByUser(String username) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+        return watchRepository.findByUser(user);
+    }
+
+    public WatchList saveWatchList(WatchList watchList) {
+        return watchRepository.save(watchList);
+    }
+    public void deleteWatchList(WatchList watchList) {
+        watchRepository.delete(watchList);
+    }
+}
