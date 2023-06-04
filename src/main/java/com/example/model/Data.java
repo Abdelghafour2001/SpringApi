f8491a7af4ab814886b44a364e58df53995971c9
Abdelghafour2001 <abdelghafourlahrache@gmail.com>
2023-06-04 23:08:28 +0200

    Admin services

---
+++ src/main/java/com/example/model/Data.java
@@ -0,0 +1,16 @@
+package com.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Data {
+    String currentPage;
+    String hasNextPage;
+    Movie[] results;
+}
---
+++ src/main/java/com/example/model/ListData.java
@@ -0,0 +1,15 @@
+package com.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ListData {
+    Data data;
+
+}
