3ef927d7d9887ad5efb1dc82da6941196a6c23db
zikohax <zikohax@gmail.com>
2023-06-06 20:17:33 +0100

    Admin Service

--- src/main/java/com/example/controllers/AdminController.java
+++ src/main/java/com/example/controllers/AdminController.java
@@ -1,35 +1,10 @@
 package com.example.controllers;
 
-import com.example.service.AdminService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/admin")
+@RequestMapping("/admin")
 public class AdminController {
-
-    private final AdminService adminService;
-
-    public AdminController(AdminService adminService) {
-        this.adminService = adminService;
-    }
-
-    @DeleteMapping("/posts/{postId}")
-    public ResponseEntity<String> deletePost(@PathVariable Long postId) {
-        adminService.deletePost(postId);
-        return ResponseEntity.status(HttpStatus.OK).body("Post deleted successfully");
-    }
-
-    @DeleteMapping("/users/{userId}")
-    public ResponseEntity<String> deleteUser(@PathVariable Long userId) {
-        adminService.deleteUser(userId);
-        return ResponseEntity.status(HttpStatus.OK).body("User deleted successfully");
-    }
-
-    @DeleteMapping("/comments/{commentId}")
-    public ResponseEntity<String> deleteComment(@PathVariable Long commentId) {
-        adminService.deleteComment(commentId);
-        return ResponseEntity.status(HttpStatus.OK).body("Comment deleted successfully");
-    }
 }
--- src/main/java/com/example/service/AdminService.java
+++ src/main/java/com/example/service/AdminService.java
@@ -2,6 +2,7 @@ package com.example.service;
 
 import com.example.dto.CommentsDto;
 import com.example.model.Comment;
+import com.example.model.Post;
 import com.example.model.User;
 import com.example.repository.CommentRepository;
 import com.example.repository.PostRepository;
@@ -32,18 +33,24 @@ public class AdminService {
     public void deletePost(Long postId) {
         postRepository.deleteById(postId);
     }
+    public Post getPostById(Long postId){
+        return postRepository.findByPostId(postId);
+    }
     public void deleteSubreddit(Long subredditId){
         subredditRepository.deleteById(subredditId);
     }
 
-
     public void deleteUser(Long userId) {
         userRepository.deleteById(userId);
     }
-    public void disableUser(String email){
-        User user = userRepository.findByEmail(email);
-        user.setEnabled(false);
-        userRepository.save(user);
+    public void disableUser(String username){
+        Optional<User> user = userRepository.findByUsername(username);
+        if (user.isPresent()){
+            User user1 = user.get();
+            user1.setEnabled(false);
+            userRepository.save(user1);
+        }
+
     }
     public List<User> getAllUsers() {
         return userRepository.findAll();
