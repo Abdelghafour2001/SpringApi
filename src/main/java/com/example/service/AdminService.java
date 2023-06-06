ace12c2fa181f252159c742c28f269a4351e2e52
zikohax <zikohax@gmail.com>
2023-06-06 19:32:31 +0100

    Admin Service

--- src/main/java/com/example/service/AdminService.java
+++ src/main/java/com/example/service/AdminService.java
@@ -1,33 +1,56 @@
 package com.example.service;
 
+import com.example.dto.CommentsDto;
+import com.example.model.Comment;
+import com.example.model.User;
 import com.example.repository.CommentRepository;
 import com.example.repository.PostRepository;
+import com.example.repository.SubredditRepository;
 import com.example.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @Service
 public class AdminService {
+    @Autowired
+    private  PostRepository postRepository;
+    private  UserRepository userRepository;
+    private  CommentRepository commentRepository;
+    private SubredditRepository subredditRepository;
 
-    private final PostRepository postRepository;
-    private final UserRepository userRepository;
-    private final CommentRepository commentRepository;
 
-    public AdminService(PostRepository postRepository, UserRepository userRepository, CommentRepository commentRepository) {
-        this.postRepository = postRepository;
-        this.userRepository = userRepository;
-        this.commentRepository = commentRepository;
+
+    public AdminService() {
+
     }
+
 
     public void deletePost(Long postId) {
         postRepository.deleteById(postId);
     }
+    public void deleteSubreddit(Long subredditId){
+        subredditRepository.deleteById(subredditId);
+    }
+
 
     public void deleteUser(Long userId) {
         userRepository.deleteById(userId);
     }
-
+    public void disableUser(String email){
+        User user = userRepository.findByEmail(email);
+        user.setEnabled(false);
+        userRepository.save(user);
+    }
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
     public void deleteComment(Long commentId) {
         commentRepository.deleteById(commentId);
     }
+    public List<Comment> getAllcomments(){return commentRepository.findAll();}
+
 }
